c96a9cc4e32009bc40fa2c61ef132045cd358ab0
Emiya-syw <syw95@mail.ustc.edu.cn>
2024-04-15 06:48:20 +0000

    update

--- scripts/nice_2_merge.py
+++ scripts/nice_2_merge.py
@@ -5,10 +5,14 @@ import csv
 import random
 dir = "./playground/data/nice_2"
 csv_file_path = os.path.join(dir, "candidate_captions.csv")
-json_file_list_0 = sorted(glob.glob(dir+"/choices_0_*.json"))
+json_file_list_0 = sorted(glob.glob(dir+"/choices_vgpt_0_*.json"))
 json_file_list_0.sort(key=lambda x: os.path.getmtime(x))
-json_file_list_1 = sorted(glob.glob(dir+"/choices_1_*.json"))
+json_file_list_1 = sorted(glob.glob(dir+"/choices_vgpt_1_*.json"))
 json_file_list_1.sort(key=lambda x: os.path.getmtime(x))
+json_file_list_2 = sorted(glob.glob(dir+"/choices_vgpt_2_*.json"))
+json_file_list_2.sort(key=lambda x: os.path.getmtime(x))
+json_file_list_3 = sorted(glob.glob(dir+"/choices_vgpt_3_*.json"))
+json_file_list_3.sort(key=lambda x: os.path.getmtime(x))
 
 
 def add_samples(json_samples, json_file_list):
@@ -20,24 +24,30 @@ def add_samples(json_samples, json_file_list):
 json_samples = []
 json_samples = add_samples(json_samples, json_file_list_0)
 json_samples = add_samples(json_samples, json_file_list_1)
-answers = {}
+json_samples = add_samples(json_samples, json_file_list_2)
+json_samples = add_samples(json_samples, json_file_list_3)
 
+answers = {}
+num_choice = 0
 with open(csv_file_path, 'r', encoding='utf-8', errors='ignore') as f:
         csv_file = csv.DictReader(f)
         for i, row in enumerate(csv_file):
-            json_samples[i]["id"] = int(row["filename"].split('.')[0])
-            choice = json_samples[i]["choice"]
-            key = "caption"+str(choice)
-            if len(row[key]) == 0:
-                key = "caption"+str(random.randint(1,63))
-            json_samples[i]["caption"] = row[key]
-            answers[json_samples[i]["id"]] = json_samples[i]["caption"]
+            # json_samples[i]["id"] = int(row["filename"].split('.')[0])
+            choices = json_samples[i]["choice"]
+            for choice in choices:
+                key = "caption"+str(choice+1)
+                if len(row[key]) == 0:
+                    continue
+                json_samples[i][choice] = row[key]
+                num_choice += 1
+                if num_choice == 5:
+                    break
 
 with open(os.path.join(dir, "merge.json"), 'w') as f:
     json.dump(json_samples, f)
     
-with open(os.path.join(dir, "answers.json"), 'w') as f:
-    json.dump(answers, f)
+# with open(os.path.join(dir, "answers.json"), 'w') as f:
+#     json.dump(answers, f)
 
 
 
